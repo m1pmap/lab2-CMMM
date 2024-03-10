d6824889c86e7def5f8e45c1f8e30885ff9b5cba
m1pmap <dok_koks@mail.ru>
2024-03-11 00:47:52 +0300

    Добавьте файлы проекта.

---
+++ lab 2/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(820, 254);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Методы решения";
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(18, 169);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(742, 64);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Метод деления отрезка пополам";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(18, 114);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(466, 64);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Метод касательных";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(18, 55);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(287, 64);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Метод хорд";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 60);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Условие:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(236, 279);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(596, 53);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(311, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(521, 71);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Решить задачу";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(20, 423);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 60);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ответ:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(179, 430);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(653, 53);
+            this.textBox2.TabIndex = 5;
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.chart1.BackSecondaryColor = System.Drawing.SystemColors.ActiveCaption;
+            this.chart1.BorderlineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+            this.chart1.BorderlineWidth = 10;
+            this.chart1.BorderSkin.BackColor = System.Drawing.Color.Black;
+            this.chart1.BorderSkin.BorderWidth = 10;
+            chartArea5.AxisX.ScaleView.SmallScrollMinSize = 0.1D;
+            chartArea5.AxisX.ScaleView.SmallScrollSize = 0.1D;
+            chartArea5.CursorX.Interval = 0.1D;
+            chartArea5.CursorX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea5.CursorX.IsUserEnabled = true;
+            chartArea5.CursorX.IsUserSelectionEnabled = true;
+            chartArea5.CursorY.Interval = 0.1D;
+            chartArea5.CursorY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            this.chart1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(30, 509);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            this.chart1.PaletteCustomColors = new System.Drawing.Color[] {
+        System.Drawing.SystemColors.ActiveCaption};
+            series5.BorderWidth = 5;
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            series5.Legend = "Legend1";
+            series5.Name = "График функции";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(802, 518);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(20, 351);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 60);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "От:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(91, 355);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(57, 53);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Impact", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox4.Location = new System.Drawing.Point(237, 355);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(57, 53);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Impact", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(154, 351);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 60);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "До:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.ClientSize = new System.Drawing.Size(844, 1039);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ lab 2/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using Antlr.Runtime;
+using NCalc;
+
+namespace lab_2
+{
+    public partial class Form1 : Form
+    {
+        public double a;
+        public double b;
+        public double eps = 0.0001;
+        public double answer;
+
+        string myFunction;
+
+        public double f(double x, string function)
+        {
+            Expression y = new Expression(function);
+            y.Parameters["x"] = x;
+            object res = y.Evaluate();
+            return Convert.ToDouble(res);
+        }
+
+        static double derivative(double x, string function, double h)
+        {
+            Expression y = new Expression(function);
+            y.Parameters["x"] = x;
+
+            double fx = (double)y.Evaluate();
+
+            y.Parameters["x"] = x - h;
+            double fxMinusH = (double)y.Evaluate();
+
+            return (fx - fxMinusH) / h;
+        }
+
+        public double MH(double a, double b, double eps)
+        {
+            double x0;
+            double x1 = a;
+            double x2 = b;
+            int i = 1;
+            answer = 0;
+
+            try
+            {
+                while (a < b)
+                {
+                    chart1.Series[0].Points.AddXY(a, f(a, myFunction));
+                    a += 0.1;
+                }
+
+                while (Math.Abs(x2 - x1) > eps)
+                {
+                    x0 = x1;
+                    x1 = x2;
+                    x2 = x1 - ((x0 - x1) * f(x1, myFunction)) / (f(x0, myFunction) - f(x1, myFunction));
+
+                    chart1.Series.Add("Хорда " + i.ToString());
+                    chart1.Series[i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    chart1.Series[i].Points.AddXY(b, f(b, myFunction));
+                    chart1.Series[i].Points.AddXY(x2, f(x2, myFunction));
+                    chart1.Series[i].BorderWidth = 3;
+                    i++;
+                }
+
+                answer = x2;
+            }
+            catch
+            {
+                MessageBox.Show("Функция введена неверно");
+            }
+            return answer;
+        }
+
+        public double MN(double a, double b, double eps)
+        {
+            double x1 = a;
+            double x0 = a + 2 * eps;
+            double x = a;
+            int i = 1;
+            answer = 0;
+
+
+            try
+            {
+                while (a < b)
+                {
+                    chart1.Series[0].Points.AddXY(a, f(a, myFunction));
+                    a += 0.1;
+                }
+
+                while (Math.Abs(x0 - x1) > eps)
+                {
+                    chart1.Series.Add("Касательная " + i.ToString());
+                    chart1.Series[i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    chart1.Series[i].Points.AddXY(x1, f(x1, myFunction));
+
+                    x0 = x1;
+                    x1 = x0 - (f(x0, myFunction) / derivative(x0, myFunction, eps));
+
+                    chart1.Series[i].Points.AddXY(x1, 0);
+                    chart1.Series[i].BorderWidth = 3;
+                    i++;
+                }
+                answer = x1;
+            }
+            catch
+            {
+                MessageBox.Show("Функция введена неверно!");
+            }
+            return answer;
+        }
+
+        public double MDSH(double a, double b, double eps)
+        {
+            double c;
+            double x1 = a;
+            double x2 = b;
+            int i = 1;
+            answer = 0;
+
+            try
+            {
+                while (a < b)
+                {
+                    chart1.Series[0].Points.AddXY(a, f(a, myFunction));
+                    a += 0.1;
+                }
+
+                while (Math.Abs(x2 - x1) > eps)
+                {
+                    c = (x1 + x2) / 2.0;
+                    if (f(x1, myFunction) * f(c, myFunction) > 0)
+                        x1 = c;
+                    else
+                        x2 = c;
+
+                    chart1.Series.Add(i.ToString());
+                    chart1.Series[i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    chart1.Series[i].Points.AddXY(c, f(c, myFunction));
+                    chart1.Series[i].Points.AddXY(c, 0);
+                    chart1.Series[i].BorderDashStyle = ChartDashStyle.Dash;
+                    i++;
+
+                    chart1.Series.Add(i.ToString());
+                    chart1.Series[i].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    chart1.Series[i].Points.AddXY(c, f(c, myFunction));
+                    chart1.Series[i].Points.AddXY(0, f(c, myFunction));
+                    chart1.Series[i].BorderDashStyle = ChartDashStyle.Dash;
+                    i++;
+
+                    chart1.Legends.Clear();
+                }
+
+                answer = (x1 + x2) / 2;
+            }
+            catch
+            {
+                MessageBox.Show("Функция введена неверно");
+            }
+            return answer;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "Sqrt(1 - 0.4*Pow(x, 2)) - Asin(x)";
+            textBox3.Text = "0";
+            textBox4.Text = "1";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "3*x - 4*Log(x, Exp(1)) - 5";
+            textBox3.Text = "0,1";
+            textBox4.Text = "2";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "Exp(x) - Exp(-x) - 2";
+            textBox3.Text = "-1";
+            textBox4.Text = "2";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            chart1.Series.Add("График функции");
+            chart1.Series[0].BorderWidth = 5;
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
+            myFunction = textBox1.Text;
+            a = Convert.ToDouble(textBox3.Text);
+            b = Convert.ToDouble(textBox4.Text);
+
+            if (radioButton1.Checked == true)
+            {
+                textBox2.Text = MH(a, b, eps).ToString();
+            }
+            if (radioButton2.Checked == true)
+            {
+                textBox2.Text = MN(a, b, eps).ToString();
+            }
+            if (radioButton3.Checked == true)
+            {
+                textBox2.Text = MDSH(a, b, eps).ToString();
+            }
+        }
+    }
+}
